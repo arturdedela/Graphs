fd04ea006c8944c43ad67c2f3f8f2aaae0640388
Artur Dedela <arturdedela@gmail.com>
2019-03-27 20:22:56 +0300

    Remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 
-import "./App.scss";
-import Counter from "./components/Counter/Counter";
-
 
 class App extends React.Component {
 
     public render() {
         return (
             <div>
-                <h1>React-Mobx-Typescript</h1>
-                <Counter />
+              Graphs
             </div>
         );
     }
